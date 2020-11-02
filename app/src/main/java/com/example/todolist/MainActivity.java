7a8c10b2fe2a7430aad078aebdfc9f43b125f0b3
Patryk Baran <patryk.baran2.stud@pw.edu.pl>
2020-11-02 22:13:05 +0100

    added making new textview task after clicking button

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout mLayout;
+    private EditText addTaskBox;
+    private FloatingActionButton addTaskButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addTaskButton = (FloatingActionButton) findViewById(R.id.add_task_button);
+        addTaskBox = (EditText) findViewById(R.id.add_task_box);
+        mLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+    }
+
+
+
+    public void addTask(View view) {
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLayout.addView(createNewTextView(addTaskBox.getText().toString()));
+            }
+        });
+
+    }
+    private TextView createNewTextView(String text) {
+        final ConstraintLayout.LayoutParams lparams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        final TextView textView = new TextView(this);
+        textView.setLayoutParams(lparams);
+        textView.setText(text);
+        return textView;
     }
 }
