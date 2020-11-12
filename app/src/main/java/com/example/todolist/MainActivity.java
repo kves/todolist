a0e0dab936ee2acfa2179177f587d95ee7545bf5
Patryk Baran <patryk.baran2.stud@pw.edu.pl>
2020-11-12 01:43:47 +0100

    tasks are adding in a list

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -5,18 +5,31 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.layout.simple_list_item_1;
 
 public class MainActivity extends AppCompatActivity {
 
     private ConstraintLayout mLayout;
     private EditText addTaskBox;
     private FloatingActionButton addTaskButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> tasksList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,24 +39,18 @@ public class MainActivity extends AppCompatActivity {
         addTaskButton = (FloatingActionButton) findViewById(R.id.add_task_button);
         addTaskBox = (EditText) findViewById(R.id.add_task_box);
         mLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
-    }
-
-
-
-    public void addTask(View view) {
+        listView = (ListView) findViewById(R.id.listView);
+        tasksList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tasksList);
+        listView.setAdapter(adapter);
         addTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mLayout.addView(createNewTextView(addTaskBox.getText().toString()));
+                tasksList.add(addTaskBox.getText().toString());
+                adapter.notifyDataSetChanged();
             }
         });
 
     }
-    private TextView createNewTextView(String text) {
-        final ConstraintLayout.LayoutParams lparams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
-        final TextView textView = new TextView(this);
-        textView.setLayoutParams(lparams);
-        textView.setText(text);
-        return textView;
-    }
+
 }
